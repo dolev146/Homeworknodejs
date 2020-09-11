4c684ae01bb9edcd3cd214142129a06812ec9cbe
dolev146 <62290677+dolev146@users.noreply.github.com>
2020-09-11 08:33:10 +0300

    finished homework for daniel

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var url = require('url')
 var path = require('path')
 var http = require("http")
 let employees = require("./employees.json")
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 
 console.log(employees)
 
@@ -21,18 +24,34 @@ app.get("/api/employees/:id", (req, res) => {
     let employee = employees.filter((employee) => {
         return employee.id === +id
     })
-    if (employee) {
+    if (employee.length === 0 ) {
         employee = { eror: 404, note: "sorry we cant find the employee" }
     }
     res.json(employee)
 })
 
+app.get('/api/employees/' , (req , res) =>{
+    console.log("Query" , req.query)
+    const { id } = req.query
+    let employee = employees.filter((employee) => {
+        return employee.id === +id
+    })
+    if (!employee) {
+        employee = { eror: 404, note: "sorry we cant find the employee" }
+    }
+    res.json(employee)
+} )
+
 app.delete("/api/employees/:id", (req, res) => {
+    
     const { id } = req.params
     employees = employees.filter((employee) => {
         return employee.id !== +id
     })
     res.json(employees)
 })
+
+
+
 
 app.listen(3000, () => console.log('run on port 3000'));
